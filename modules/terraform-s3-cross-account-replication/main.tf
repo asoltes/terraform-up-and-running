f79eb1a7d4d826d120ddb4264cbe0232b31730e0
Andrew Soltes <soltesandrew@gmail.com>
2024-02-17 15:57:00 +0800

    your commit messagen

--- modules/terraform-s3-cross-account-replication/main.tf
+++ modules/terraform-s3-cross-account-replication/main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   region = "ap-southeast-1"
   profile = "jenkins"
-  shared_credentials_files = ["/home/jenkins/.aws/config"]
+  shared_credentials_files = ["/var/lib/jenkins/.aws/config"]
 }
 
 data "aws_caller_identity" "current" {}
