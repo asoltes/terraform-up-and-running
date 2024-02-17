be4b64231a131eb39c350a4fc129854d3d40e6aa
Andrew Soltes <soltesandrew@gmail.com>
2024-02-17 14:57:51 +0800

    your commit messagen

--- modules/terraform-s3-cross-account-replication/main.tf
+++ modules/terraform-s3-cross-account-replication/main.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 
 }
 
